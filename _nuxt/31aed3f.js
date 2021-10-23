df9d824881b94cd99c9cc0e8312af393fecb5d3e
urbontaitis <urbontaitis@users.noreply.github.com>
2021-10-23 17:08:07 +0000

    deploy: f49716e7771dd77d8343df57a709c2cc64a7cac5

---
+++ _nuxt/31aed3f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{252:function(e,t,n){"use strict";n.r(t);var r={},o=n(8),component=Object(o.a)(r,(function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("article",[n("header",[e._t("header")],2),e._v(" "),e._t("body"),e._v(" "),n("footer",[e._t("footer")],2)],2)}),[],!1,null,null,null);t.default=component.exports}}]);
