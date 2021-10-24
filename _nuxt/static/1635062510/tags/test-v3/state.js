e97fcb0c848e69a07b948f4b9b5f3bbb61d7009a
urbontaitis <urbontaitis@users.noreply.github.com>
2021-10-24 08:07:31 +0000

    deploy: d478969e5b43d423b2736b53bf1dcff95a1cb23a

--- _nuxt/static/1635062510/tags/test-v3/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635062510",layout:"blog",error:a,serverRendered:true,routePath:"\u002Ftags\u002Ftest-v3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"195e2c5f"}}}}(null));
