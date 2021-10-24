00ccf5e7e5dbec30cd8edb0c340532b2542cb030
urbontaitis <urbontaitis@users.noreply.github.com>
2021-10-24 18:28:44 +0000

    deploy: c72a00df5b264ddfd71f5e5cba8fc65797758bcf

---
+++ _nuxt/static/1635100123/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635100123",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4535948d"}}}}(null,"\u002F"));
