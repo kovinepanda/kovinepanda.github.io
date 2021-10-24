5b19e8efe063325dc3536fdc5b28999971e0b653
urbontaitis <urbontaitis@users.noreply.github.com>
2021-10-24 15:55:33 +0000

    deploy: b1a20a6c56922065ca31739fd8d3772b266a4026

--- _nuxt/static/1635088843/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635088843",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9a5338d1"}}}}(null,"\u002F"));
--- _nuxt/static/1635088843/tags/test/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635088843",layout:"blog",error:a,serverRendered:true,routePath:"\u002Ftags\u002Ftest",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9a5338d1"}}}}(null));
