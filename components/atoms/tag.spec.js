0d10b87275a837be34fb0dc2d1d3b4d53dbe0ae9
Mindaugas <mindaugas@urbontaitis.lt>
2021-10-24 21:41:01 +0300

    verify if hastag is added

--- components/atoms/tag.spec.js
+++ components/atoms/tag.spec.js
@@ -2,12 +2,24 @@ import { mount } from "@vue/test-utils";
 import Tag from './tag';
 
 describe('Tag', () => {
+
+    it('renders hashtag before slug', () => {
+        const wrapper = mount(Tag, {
+            propsData: {
+                slug: 'my-hashtag'
+            }
+        });
+
+        expect(wrapper.text()).toContain('#my-hashtag');        
+    })
+
     it('renders correctly', () => {
         const wrapper = mount(Tag, {
             propsData: {
                 slug: 'vue 🤘'
             }
         })
+
         expect(wrapper.element).toMatchSnapshot()
-      })
+    })
 })
